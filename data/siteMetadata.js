bb6401a5245096b957d6ffd6d16dde43b9210bfb
minhlk <duyminhdev@gmail.com>
2023-10-10 12:11:10 +0700

    [MOD] Custom logo and information

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: 'MinhLK Blog',
   author: 'MinhLK',
   headerTitle: 'MinhLK Blog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'MinhLK Personal Blog',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
