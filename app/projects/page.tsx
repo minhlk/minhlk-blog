a4a7563cf446ebb022121bb08ea284f8273d4260
minhlk <duyminhdev@gmail.com>
2023-10-11 10:34:13 +0700

    [MOD] Update project list

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,17 @@
-import projectsData from '@/data/projectsData'
+// import projectsData from '@/data/projectsData'
 import Card from '@/components/Card'
 import { genPageMetadata } from 'app/seo'
 
 export const metadata = genPageMetadata({ title: 'Projects' })
 
-export default function Projects() {
+export default async function Projects() {
+  const page1 = await (
+    await fetch('https://api.github.com/users/minhlk/repos?sort=created-desc')
+  ).json()
+  const page2 = await (
+    await fetch('https://api.github.com/users/minhlk/repos?sort=created-desc&page=2')
+  ).json()
+  const projectsData = [...page1, ...page2]
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
@@ -13,18 +20,19 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            A list of projects I have been working on or built
           </p>
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
             {projectsData.map((d) => (
               <Card
-                key={d.title}
-                title={d.title}
+                key={d.name}
+                title={d.name}
                 description={d.description}
-                imgSrc={d.imgSrc}
-                href={d.href}
+                href={d.svn_url}
+                topics={d.topics}
+                star={d.stargazers_count}
               />
             ))}
           </div>
